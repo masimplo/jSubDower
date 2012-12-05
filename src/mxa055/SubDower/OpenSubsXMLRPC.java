06a3a55212bdd6799c3c3377638cf09514c4aac8
Michael <mxa055@gmail.com>
2012-12-05 02:33:48 +0200

    Completed ServerInfo opensubtitles function

--- src/mxa055/SubDower/OpenSubsXMLRPC.java
+++ src/mxa055/SubDower/OpenSubsXMLRPC.java
@@ -6,10 +6,13 @@ package mxa055.SubDower;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.*; 
 import org.junit.Assert;
+
+import sun.security.util.Debug;
 
 public class OpenSubsXMLRPC {
 	private XmlRpcClient server;
@@ -26,8 +29,35 @@ public class OpenSubsXMLRPC {
 		Object[] params = new Object[0];
 		HashMap<String,Object> response = (HashMap<String, Object>) server.execute("ServerInfo", params);
 		Assert.assertNotNull(response);
-		boolean works = response.containsKey("xmlrpc_version");
-		Object version = response.get("xmlrpc_version");
-		Assert.assertNotNull(version);
+		
+		Debug.println("Version", (String) response.get("xmlrpc_version"));
+		Debug.println("Url", (String) response.get("xmlrpc_url"));
+		Debug.println("Application", (String) response.get("application"));
+		Debug.println("Contact",(String) response.get("contact"));
+		Debug.println("Website url",(String) response.get("website_url"));
+		Debug.println("Total Users online", String.valueOf((int) response.get("users_online_total")));
+		Debug.println("Program Users online", String.valueOf((int) response.get("users_online_program")));
+		Debug.println("Users logged in", String.valueOf((int) response.get("users_loggedin")));
+		Debug.println("Maximum users online", (String) response.get("users_max_alltime"));
+		Debug.println("Registered users", (String) response.get("users_registered"));
+		Debug.println("Sub downloads", (String) response.get("subs_downloads"));
+		Debug.println("Subtitles files", (String) response.get("subs_subtitle_files"));
+		Debug.println("Total movies", (String) response.get("movies_total"));
+		Debug.println("Movies aka", (String) response.get("movies_aka"));
+		Debug.println("Total subtitle languages", (String) response.get("total_subtitles_languages"));
+		Debug.println("Seconds",  String.valueOf((double)response.get("seconds")));
+		
+		HashMap<String, Object> lastUpdateStrings =  (HashMap<String, Object>) response.get("last_update_strings");
+		Debug.println("GR", (String) lastUpdateStrings.get("el"));
+		Debug.println("EN", (String) lastUpdateStrings.get("en"));
+		
+		
+		HashMap<String, Object> downloadLimits =  (HashMap<String, Object>) response.get("download_limits");
+		Debug.println("Global 24 Hour download limit", String.valueOf((int) downloadLimits.get("global_24h_download_limit")));
+		Debug.println("Client IP", (String) downloadLimits.get("client_ip"));
+		Debug.println("Limit check by", (String) downloadLimits.get("limit_check_by"));
+		Debug.println("Client 24h download count", String.valueOf((int) downloadLimits.get("global_24h_download_limit")));
+		Debug.println("Client download quota", String.valueOf((int) downloadLimits.get("client_download_quota")));
+		Debug.println("Client 24 Hour download limit", String.valueOf((int) downloadLimits.get("client_24h_download_limit")));		  
 	}
 }
